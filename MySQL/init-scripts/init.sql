2cc616c8eb01a38443c91ecdf13fcca483d91f8f
Erick Gabriel Brunoro Mesquita <erick.mesquita@usp.br>
2022-03-04 19:20:07 -0300

    Aspas simples no nome do usuário 'app'

--- MySQL/init-scripts/init.sql
+++ MySQL/init-scripts/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE Medidas CHARACTER SET utf8;
 USE Medidas;
-GRANT SELECT, INSERT ON Medidas.* TO app@'%';
+GRANT SELECT, INSERT ON Medidas.* TO 'app'@'%';
 CREATE TABLE Temperatura(
     Horario TIMESTAMP NOT NULL UNIQUE PRIMARY KEY COMMENT 'Horário de coleta das medidas.',
     Temperatura DECIMAL(4, 2) COMMENT 'Temperatura medida em graus centígrados.',
