80abff844e8e21a63852c6dafcf3a3a4ad8fd857
Erick Gabriel Brunoro Mesquita <erick.mesquita@usp.br>
2022-03-04 19:40:34 -0300

    Campos DECIMAL alterados para FLOAT

--- MySQL/init-scripts/init.sql
+++ MySQL/init-scripts/init.sql
@@ -3,8 +3,8 @@ USE Medidas;
 GRANT SELECT, INSERT ON Medidas.* TO 'app'@'%';
 CREATE TABLE Temperatura(
     Horario TIMESTAMP NOT NULL UNIQUE PRIMARY KEY COMMENT 'Horário de coleta das medidas.',
-    Temperatura DECIMAL(4, 2) COMMENT 'Temperatura medida em graus centígrados.',
-    Umidade DECIMAL(4, 2) UNSIGNED COMMENT 'Umidade relativa do ar em porcentagem.'
+    Temperatura FLOAT COMMENT 'Temperatura medida em graus centígrados.',
+    Umidade FLOAT UNSIGNED COMMENT 'Umidade relativa do ar em porcentagem.'
 );
 
 
